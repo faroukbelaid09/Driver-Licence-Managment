e8190af828b7926fbdfad5f42561981308218bda
Farouk <faroukbelaid9@gmail.com>
2025-08-25 13:37:17 +0300

    Create Driving License info usre control

---
+++ DrivingLicense/InternationalLicenseForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace DrivingLicense
+{
+    partial class InternationalLicenseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchBTN = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(240, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "International License Application";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.searchBTN);
+            this.groupBox1.Controls.Add(this.searchTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(516, 76);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "License ID:";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(166, 25);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(175, 23);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // searchBTN
+            // 
+            this.searchBTN.Location = new System.Drawing.Point(364, 22);
+            this.searchBTN.Name = "searchBTN";
+            this.searchBTN.Size = new System.Drawing.Size(75, 28);
+            this.searchBTN.TabIndex = 2;
+            this.searchBTN.Text = "Search";
+            this.searchBTN.UseVisualStyleBackColor = true;
+            // 
+            // InternationalLicenseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "InternationalLicenseForm";
+            this.Text = "InternationalLicenseForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button searchBTN;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ DrivingLicense/ctrlDriverLicenseInfo.cs
@@ -0,0 +1,38 @@
+﻿using DrivingLicenseBusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrivingLicense
+{
+    public partial class ctrlDriverLicenseInfo : UserControl
+    {
+        public ctrlDriverLicenseInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDate(clsFullLicenseDetails fullLicernse)
+        {
+            ClassValue.Text = fullLicernse.ClassName;
+            NameValue.Text = fullLicernse.FullName;
+            LicenseIDValue.Text = fullLicernse.LicenseID.ToString();
+            IsActiveValue.Text = fullLicernse.IsActive.ToString();
+            NationalNoValue.Text = fullLicernse.NationalNo;
+            DateOfBirthValue.Text = fullLicernse.DateOfBirth;
+            GenderValue.Text = fullLicernse.Gender;
+            DriverIDValue.Text = fullLicernse.DiverID.ToString();
+            IssueDateValue.Text = fullLicernse.IssueDate;
+            ExpirationDateValue.Text = fullLicernse.ExpirationDate;
+            IssueReasonValue.Text = fullLicernse.IssueReason == 1 ? "First Time" : "Others";
+            IsDetainedValue.Text = fullLicernse.IsDetained.ToString();
+            NotesValue.Text = fullLicernse.Notes;
+        }
+    }
+}
